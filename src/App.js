d1a53a056a32612bb5981aa0705461dd5c483613
maxohsawa <max.ohsawa@gmail.com>
2021-09-21 09:30:38 -0400

    complete use callback tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+import List from './List.js';
 
-function App() {
+export default function App() {
+  const [number, setNumber] = useState(1);
+  const [dark, setDark] = useState(false);
+
+  const getItems = useCallback(() => {
+    return [number, number+1, number+2];
+  }, [number]);
+
+  const theme = {
+    backgroundColor: dark ? '#333' : '#FFF',
+    color: dark ? '#FFF' : '#333'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={theme}>
+      <input 
+        type="number" 
+        value={number} 
+        onChange={e => setNumber(parseInt(e.target.value))}  
+      />
+      <button onClick={() => setDark(prevDark => !prevDark)}>
+        Toggle Theme
+      </button>
+      <List getItems={getItems} />
     </div>
   );
 }
-
-export default App;
